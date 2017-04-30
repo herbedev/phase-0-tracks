9542c225c466726390e8fb5ef5690900a76a7d24
John Herbener <herbener.dev@gmail.com>
2017-04-30 14:07:30 -0400

    pseudocode for interior designer client program

---
+++ ruby/hashes.rb
@@ -0,0 +1,14 @@
+# PSEUDOCODE
+# 1. Initalize Hash
+# 2. Ask the designer / user for all of the required information
+# 3. Comvert any user input to the appropriate data type
+# 4. Print the hash back out to the screen when the designer has answered all of the questions
+# 5. Give the user the opportunity to update a key (no need to loop) If the designer says "none", skip it
+# 	5.1 If the user responds with no, proceed to the end.
+# 	5.2 If the user responds with a key they would like to update
+# 		5.21 Have the user enter the corresponding key
+#  		5.22 Replace value in that key with the new user input
+# 6. Print the latest version of the hash, and exit the program.
+
+# 1. Initalize Hash
+
