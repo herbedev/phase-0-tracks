1aa1c0ec1e767791c6273ae4e0d5d9d7967e3af5
John Herbener <herbener.dev@gmail.com>
2017-05-08 15:57:56 -0400

    update santa class with attributes: gender, ethnicity, reindeer_ranking, and age

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,4 +1,15 @@
 class Santa
+
+  #initialize method
+  def initialize(gender, ethnicity)
+  	@gender = gender
+  	@ethnicity = ethnicity
+  	@age = 0
+  	reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+  	puts "Initializing Santa instance #{@gender}, #{@ethnicity} ..."
+
+  end
+
   #Speak Method
   def speak
   	puts "Ho, ho, ho! Haaaappy holidays!"
@@ -9,13 +20,32 @@ class Santa
   	puts "That was a good #{cookie_type}"
   end
 
-  #initialize method
-  def initialize
-  	puts "Initializing Santa instance ..."
-  end
-
 end
 
-santa_one = Santa.new
-santa_one.speak
-santa_one.eat_milk_and_cookies("snickerdoodle")
+# Release 0 Driver code
+# santa_one = Santa.new
+# santa_one.speak
+# santa_one.eat_milk_and_cookies("snickerdoodle")
+
+# Release 1 Driver code
+# Code below is too repetitive, so lets try something else
+
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+santas = []
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_genders.length.times do |i|
+  santas << Santa.new(example_genders[i], example_ethnicities[i])
+end
+
+
+
+
