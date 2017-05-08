2ad6f13f4bb7fe082a221dcc7d534afff9d59928
John Herbener <herbener.dev@gmail.com>
2017-05-08 17:44:37 -0400

    write two classes that represent anything that might should, and include the shout module in those classes

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,11 +1,38 @@
+# module Shout
+#    def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+#    def self.yell_happily(words)
+#     words + "!!!" + " :)"
+#   end
+# end
+
+# p Shout.yell_happily("HAPPY")
+# p Shout.yell_angrily("ANGRY")
+
 module Shout
-   def self.yell_angrily(words)
+
+   def yell_angrily(words)
     words + "!!!" + " :("
   end
-   def self.yell_happily(words)
+   def yell_happily(words)
     words + "!!!" + " :)"
   end
+
 end
 
-p Shout.yell_happily("HAPPY")
-p Shout.yell_angrily("ANGRY")
+class Angry_Person
+	include Shout
+end
+
+class Happy_Person
+	include Shout
+end
+
+Angry_Person_Instance = Angry_Person.new
+p Angry_Person_Instance.yell_angrily("NOO")
+p Angry_Person_Instance.yell_happily("YESS")
+
+Happy_Person_Instance = Happy_Person.new
+p Happy_Person_Instance.yell_happily("YESS")
+p Happy_Person_Instance.yell_angrily("NOO")
