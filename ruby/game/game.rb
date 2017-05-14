948ac2279c7b4c857148ce623c0b0096f609a523
John Herbener <herbener.dev@gmail.com>
2017-05-14 16:09:08 -0400

    game touch ups after office hours

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,23 +1,25 @@
 class Game
+  
   attr_accessor :secret_word, :secret_word_letters, :guess_limit, :guess_counter, :game_over
   
   # initialize method
   def initialize(secret_word)
-    @word = secret_word
-  	@secret_word = []
-    @word_letters = []
- 	@guess_limit = 0
+  @word = secret_word
+  @secret_word = []
+  @word_letters = secret_word.split("")
+ 	@guess_limit = @word.length * 2
  	@guess_counter = 0
  	@guessed_letters = []
- 	@game_over = FALSE
+ 	@game_over = false
  	p "initializing game instance"
+ 	encrypt_secret_word
   end
   
-  # split_secret_word method that splits user input into an array of letters
-  def split_secret_word(secret_word)
-    @word_letters = secret_word.split("")
-    return @word_letters
-  end
+  # # split_secret_word method that splits user input into an array of letters
+  # def split_secret_word(secret_word)
+  #   @word_letters = secret_word.split("")
+  #   return @word_letters
+  # end
   
   # encrypt_secret_word method that creates a new array with the exact amount of letters contained in the word
   def encrypt_secret_word
@@ -28,61 +30,52 @@ class Game
     return @secret_word
   end
   
-  # set guess limit
-  def set_guess_limit
-    @guess_limit = @word.length * 2
-  end
-  
   #method for guessing letters from secret word
-  def guess_letter(letter)
-    if @word_letters.include?(letter)
-      @word_letters.each_index.select { |x| @word_letters[x] == letter}.each do |index|
-          @secret_word[index].replace(letter)
-      end
-    end
+  # See if a letter is inside an array , if it is replace the position in another array with that letter. 
+  def guess_letter(guessed_letter)
+    # if @word_letters.include?(letter)
+
+    	@word_letters.each_with_index do |letter, index|
+		  if guessed_letter == letter 
+		  	@secret_word[index] = letter
+		  end
+		end
+      
   end
   
   def guess_counter(letter)
-  	if @guessed_letters.include?(letter) 
-  		@guess_counter
-  	else
+  	if !@word.include?(letter) 
   		@guess_counter += 1
   	end
+  	@guessed_letters.push(letter)
+  end
+  #possibly call game over in guess_counter
+
+  
+  def game_over
     if @guess_counter >= @guess_limit
-      @game_over = TRUE
+      @game_over = true
+    elsif @word == @secret_word.join("")
+      @game_over = true
     else
-      @game_over = FALSE
+      @game_over = false
     end
   end
 
-  #method for replacing letters
-  def replace_letter(letter)
-    if @word_letters == @secret_word
-      return "You hav won"
-    elsif !@word_letters.include?(letter)
-      p "Sorry no match heres the word: #{@secret_word}"
-    elsif @word_letters.include?(letter)
-        puts "Here's your updated word! #{@secret_word.join(" ")}"
-    end
+  def run
+  	user_input = ""
+  	until game_over || user_input == "done"
+  		
+	  	p "Please input a letter"
+	  	user_input = gets.chomp
+	  	guess_letter(user_input)
+	  	guess_counter(user_input)
+	end 
   end
-  
 end
-
-# while game.gameover == FALSE
-  
-# end
-
-
 #initialize new instance of game
-game1 = Game.new("baseball")
-# test if split_secret_word method works
-p game1.split_secret_word("baseball")
-# test if encrypt_secret_word method works
-p game1.encrypt_secret_word
-# test if set_guess_limit works
-p game1.set_guess_limit
-p game1.guess_letter("b")
-#p game1.replace_letter("b")
- 
+game1 = Game.new("hi")
+#
+game1.run
 
 
