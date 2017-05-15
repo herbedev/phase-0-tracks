8aa204be1ee105f9a31e8d21c081442c86267023
John Herbener <herbener.dev@gmail.com>
2017-05-15 10:13:51 -0400

    write a constructor function for a car

--- js/data_structures.js
+++ js/data_structures.js
@@ -3,3 +3,22 @@ var names = ["Ed", "Jake", "Kate", "John"]
 
 colors.push("indigo");
 names.push("Eric");
+
+var key = names[0]
+var obj = {}
+obj[key] = colors[0]
+
+console.log(obj);
+
+function Cars(make, model, year) {
+
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.accelerate = "Vroom Vroom";
+}
+
+var car = new Cars("BMW", "335", "2016");
+var car2 = new Cars("Honda", "Civic", "1995");
+console.log(car)
+console.log(car2)
